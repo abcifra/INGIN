1a2c21b7c73335448ca0453d4e15e1f1f78e3819
abcifra <d.trefilov@ing-in.ru>
2025-04-28 17:23:17 +0500

    test commit 2

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -1,6 +1,6 @@
 ﻿sealed partial class Build
 {
-    const string Version = "1.0.0";
+    const string Version = "1.0.2";
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
     readonly AbsolutePath ChangeLogPath = RootDirectory / "Changelog.md";
 
