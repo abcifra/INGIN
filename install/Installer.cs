20e978377b1b487ea757b2a509584846697fd2d6
abcifra <d.trefilov@ing-in.ru>
2025-04-28 14:39:50 +0500

    First commit

---
+++ install/Installer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Installer;
+using WixSharp;
+using WixSharp.CommonTasks;
+using WixSharp.Controls;
+using Assembly = System.Reflection.Assembly;
+
+const string outputName = "INGIN";
+const string projectName = "INGIN";
+
+var project = new Project
+{
+    OutDir = "output",
+    Name = projectName,
+    Platform = Platform.x64,
+    UI = WUI.WixUI_FeatureTree,
+    MajorUpgrade = MajorUpgrade.Default,
+    //GUID = new Guid("5B08327F-93EB-4640-96BB-934F820EDB2A"),
+    GUID = new Guid("7eba78d2-c69d-412e-9e26-ba042b7380f4"),
+    BannerImage = @"install\Resources\Icons\BannerImage.png",
+    BackgroundImage = @"install\Resources\Icons\BackgroundImage.png",
+    Version = Assembly.GetExecutingAssembly().GetName().Version.ClearRevision(),
+    ControlPanelInfo =
+    {
+        Manufacturer = Environment.UserName,
+        ProductIcon = @"install\Resources\Icons\ShellIcon.ico"
+    }
+};
+
+var wixEntities = Generator.GenerateWixEntities(args);
+project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.CustomizeDlg);
+
+BuildSingleUserMsi();
+BuildMultiUserUserMsi();
+
+void BuildSingleUserMsi()
+{
+    project.InstallScope = InstallScope.perUser;
+    project.OutFileName = $"{outputName}-{project.Version}-SingleUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
+
+void BuildMultiUserUserMsi()
+{
+    project.InstallScope = InstallScope.perMachine;
+    project.OutFileName = $"{outputName}-{project.Version}-MultiUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%CommonAppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
