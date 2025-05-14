117fbdd3c7e4c663306b3faa6980d2d6c9245408
abcifra <d.trefilov@ing-in.ru>
2025-05-14 17:00:59 +0500

    jiwejmferfmrf

--- InginUpdater/InginUpdater.cs
+++ InginUpdater/InginUpdater.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Octokit;
 
 namespace InginUpdater
 {
@@ -15,7 +14,7 @@ namespace InginUpdater
     {
         static void Main()
         {
-            Console.WriteLine("sdfksfjckdsjfkdsfsdmf");
+            Console.WriteLine("sdfksfjckdsjfkdsfssssdmf");
             Console.ReadLine();
         }
         //static void Main()
